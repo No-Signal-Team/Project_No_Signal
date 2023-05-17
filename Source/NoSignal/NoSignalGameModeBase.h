c57a71431a40fd92289c786c6f52b69ca5fad34b
nwiart <nwiart@gaming.tech>
2023-05-17 14:25:39 +0200

    Base empty project

---
+++ Source/NoSignal/NoSignalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NoSignalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOSIGNAL_API ANoSignalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
