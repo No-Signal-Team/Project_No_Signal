0990d630f6ab78d842e543dcc04db9429b30625f
tdelorme <tdelorme@gaming.tech>
2023-05-29 15:48:56 +0200

    🚧 (gameplay) : Add better lights
    * Add intelligent Point Light for stress influenced player
    * Fixed intelligent Spotlights
    * Add NS_Light abstract class to implemented as much lights as we want

--- Source/NoSignal/NS_Lights/NS_PointLight.h
+++ Source/NoSignal/NS_Lights/NS_PointLight.h
@@ -1,23 +1,23 @@
-#pragma once
+﻿#pragma once
 
-#include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
 #include "NS_Light.h"
-#include "Components/SpotLightComponent.h"
-#include "NS_Spotlight.generated.h"
+#include "CoreMinimal.h"
+#include "Components/PointLightComponent.h"
+#include "GameFramework/Actor.h"
+#include "NS_PointLight.generated.h"
 
 UCLASS()
-class NOSIGNAL_API ANS_Spotlight : public ANS_Light
+class NOSIGNAL_API ANS_PointLight : public ANS_Light
 {
 	GENERATED_BODY()
 
 public:
-	ANS_Spotlight();
+	ANS_PointLight();
 
 protected:
 	virtual bool IsInSightOfPlayer(FHitResult& OutHit) override;
 
 protected:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	USpotLightComponent* Spotlight;
+	UPointLightComponent* PointLight;
 };
--- Source/NoSignal/NS_Lights/NS_Spotlight.cpp
+++ Source/NoSignal/NS_Lights/NS_Spotlight.cpp
@@ -4,46 +4,22 @@
 #include "NS_Spotlight.h"
 
 #include "Components/SpotLightComponent.h"
-#include "Kismet/GameplayStatics.h"
 
 // Sets default values
-ANS_Spotlight::ANS_Spotlight()
+ANS_Spotlight::ANS_Spotlight() : ANS_Light()
 {
 	// Get the spotlight actor from the blueprint
 	Spotlight = CreateDefaultSubobject<USpotLightComponent>("Spotlight");
 	Spotlight->SetupAttachment(RootComponent);
-	
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void ANS_Spotlight::BeginPlay()
-{
-	Super::BeginPlay();
-	
-	// Get player character
-	PlayerCharacter = Cast<ACharacterBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
-}
-
-// Called every frame
-void ANS_Spotlight::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-	
-	FHitResult Hit;
-	if (IsInSightOfPlayer(Hit)) CompareToPlayer();
 }
 
 bool ANS_Spotlight::IsInSightOfPlayer(FHitResult& OutHit)
 {
 	if (!PlayerCharacter || !Spotlight) return false;
-	UE_LOG(LogTemp, Warning, TEXT("PlayerCharacter: %s"), *PlayerCharacter->GetName());
 	
 	// Raycast
 	const FVector Start = Spotlight->GetComponentLocation();
 	const FVector End = PlayerCharacter->GetActorLocation();
-	
 	FCollisionQueryParams Params;
 	Params.AddIgnoredActor(this);
 	const bool bHit = GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, Params);
@@ -51,19 +27,14 @@ bool ANS_Spotlight::IsInSightOfPlayer(FHitResult& OutHit)
 	// Get distance
 	const float Distance = (Start - End).Size();
 	const bool bCloseEnough = Distance <= Spotlight->AttenuationRadius;
-
-	//Log raycast and distance
-	UE_LOG(LogTemp, Warning, TEXT("Raycast: %s, Distance: %f"), bHit ? TEXT("true") : TEXT("false"), Distance);
 	
-	return !bHit && bCloseEnough;
+	// Check that the player is inside the spotlight's cone
+	const FVector Direction = (End - Start).GetSafeNormal();
+	const float ConeAngle = FMath::DegreesToRadians(Spotlight->OuterConeAngle);
+	const float DotProduct = FVector::DotProduct(Direction, Spotlight->GetForwardVector());
+	const bool bInCone = DotProduct >= FMath::Cos(ConeAngle);
+	
+	return !bHit && bCloseEnough && bInCone;
 }
 
-void ANS_Spotlight::CompareToPlayer()
-{
-	TSet<AActor*> LightsInSight = PlayerCharacter->lightAwareEntityComponent->GetLightsInSight();
-	bool bContainsLight = LightsInSight.Contains(this);
-
-	if (bContainsLight) LightsInSight.Add(this);
-	else LightsInSight.Remove(this);
-}
 
